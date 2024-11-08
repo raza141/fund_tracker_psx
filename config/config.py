f44dd0d5d2fbf8fd6d29a3bde251f9e19fdda1d1
Muhammad Ahmed Raza <razaahmed141@gmail.com>
2024-11-09 00:56:28 +0400

    Updated Code

--- config/config.py
+++ config/config.py
@@ -1,3 +1,5 @@
 FIPI_DIR = r'D:\fund_tracker_psx\data\fipi'
 LIPI_DIR = r'D:\fund_tracker_psx\data\lipi'
 PROCESSED_FIPI_LIPI_DIR = r'D:\fund_tracker_psx\data\processed_fipi_lipi'
+STOCKS_DIR = r'D:\fund_tracker_psx\data\stocks'
+RAW_STOCKS_DIR = r'D:\fund_tracker_psx\data\raw_data'
